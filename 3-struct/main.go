f304009d9961dd2b5caa23633d4ca2958ecb2a58
lex <asznamensky@rambler.ru>
2026-04-06 22:30:13 +0300

    Add packages

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
+	"demo/jsonLoader/bins"
 	"fmt"
-	"time"
 )
 
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-func NewBin(id string, private bool, name string) *Bin {
-	return &Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
-	}
-}
-
 func main() {
-	binList := []Bin{}
-	bin := NewBin("1", false, "Test")
+	binList := []bins.Bin{}
+	bin := bins.NewBin("1", false, "Test")
 	fmt.Println(bin)
 
 	binList = append(binList, *bin)
