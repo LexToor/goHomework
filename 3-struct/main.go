d0de2ed3c74a02ff40e62932f74700b3a4615c2c
Знаменский Александр <asznamensky@rambler.ru>
2026-04-09 12:59:41 +0300

    Add storage, files

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"demo/jsonLoader/bins"
+	"demo/jsonLoader/storage"
 	"fmt"
 )
 
 func main() {
-	binList := []bins.Bin{}
-	bin := bins.NewBin("1", false, "Test")
-	fmt.Println(bin)
+	binList, err := storage.LoadListBin("storage.json")
+	if err != nil {
+		fmt.Println("Ошибка чтения списка Bin-ов:", err)
+	}
 
+	bin := bins.NewBin("1", false, "Test")
 	binList = append(binList, *bin)
-	fmt.Println(binList)
+
+	err = storage.SaveListBin(&binList, "storage.json")
+	if err != nil {
+		fmt.Println("Ошибка записи списка Bin-ов:", err)
+	}
+
+	fmt.Println("binList:", binList)
+	fmt.Println("bin:", bin)
+
+	err = storage.SaveBin(bin, "bin.json")
+	if err != nil {
+		fmt.Println("Ошибка записи bin-a:", err)
+	}
+
 }
